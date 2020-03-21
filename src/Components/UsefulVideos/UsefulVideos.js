e6a0bfeec7b0456faa2d87cb37390a92e3417994
sandeep-chahal <sandeepchahal573@gmail.com>
2020-03-21 09:15:12 +0530

    fixed react error in usefulVideos

--- src/Components/UsefulVideos/UsefulVideos.js
+++ src/Components/UsefulVideos/UsefulVideos.js
@@ -11,54 +11,54 @@ const UsefulVideos = props => {
 					width="560"
 					height="315"
 					src="https://www.youtube-nocookie.com/embed/BtN-goy9VOY"
-					frameborder="0"
+					frameBorder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
+					allowFullScreen
 				></iframe>
 				<iframe
 					title="How soap kills the coronavirus"
 					width="560"
 					height="315"
 					src="https://www.youtube-nocookie.com/embed/-LKVUarhtvE"
-					frameborder="0"
+					frameBorder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
+					allowFullScreen
 				></iframe>
 				<iframe
 					title="What Coronavirus Symptoms Look Like, Day By Day"
 					width="560"
 					height="315"
 					src="https://www.youtube-nocookie.com/embed/OOJqHPfG7pA"
-					frameborder="0"
+					frameBorder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
+					allowFullScreen
 				></iframe>
 				<iframe
 					title="Coronavirus: how do I know if I'm infected and what happens next?"
 					width="560"
 					height="315"
 					src="https://www.youtube-nocookie.com/embed/2JsWf-2nN1Y"
-					frameborder="0"
+					frameBorder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
+					allowFullScreen
 				></iframe>
 				<iframe
 					title="Coronavirus outbreak (covid 19) explained through 3D Medical Animation"
 					width="560"
 					height="315"
 					src="https://www.youtube-nocookie.com/embed/I-Yd-_XIWJg"
-					frameborder="0"
+					frameBorder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
+					allowFullScreen
 				></iframe>
 				<iframe
 					title="What This Chart Actually Means for COVID-19"
 					width="560"
 					height="315"
 					src="https://www.youtube-nocookie.com/embed/fgBla7RepXU"
-					frameborder="0"
+					frameBorder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
+					allowFullScreen
 				></iframe>
 			</div>
 		</div>
