3fd050e7b520efadc41f419a2cdae60483594b05
sandeep-chahal <sandeepchahal573@gmail.com>
2020-03-21 12:19:52 +0530

    added donate btn

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,11 +4,23 @@ import "./NavBar.scss";
 const NavBar = props => {
 	return (
 		<nav className="nav">
-			<div className="nav-item">Overview</div>
-			<div className="nav-item">Charts</div>
-			<div className="nav-item">Map</div>
-			<div className="nav-item">News</div>
-			<div className="nav-item">About COVID-19</div>
+			<div className="items">
+				<div className="nav-item">Overview</div>
+				<div className="nav-item">Charts</div>
+				<div className="nav-item">Map</div>
+				<div className="nav-item">News</div>
+				<div className="nav-item">About COVID-19</div>
+				<div class="btn-container"></div>
+			</div>
+			<a
+				href="https://ko-fi.com/sandeep"
+				target="_blank"
+				className="ko-fi"
+				style={{ background: "#e02828" }}
+			>
+				<img alt="ko-fi logo" src="https://ko-fi.com/img/cup-border.png" />
+				<span>Support Me on Ko-fi</span>
+			</a>
 		</nav>
 	);
 };
